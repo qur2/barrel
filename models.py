ee5855d65fdfbab56fb6fed6e725e9ca5bf4dab1
Iurii Kudriavtsev <iurii.kudriavtsev@txtr.com>
2014-01-21 16:20:46 +0100

    SKINSTXTRCOM-4163 : `MoneyField` is not used anymore. Adding Document model.

--- models.py
+++ models.py
@@ -1,9 +1,10 @@
-from . import Store, Field, BooleanField, DateField, MoneyField, EmbeddedStoreField
+from . import Store, Field, BooleanField, DateField, IntegerField, FloatField, LongIntField, EmbeddedStoreField
 from rpc import RpcMixin, rpc_call
+from money import Money
 
 
 class Nature(Store, RpcMixin):
-    interface = 'WSTReaktor'
+    interface = 'WSReaktorMgmt'
 
     name = Field(target='name')
     home_country = Field(target='homeCountry')
@@ -16,7 +17,90 @@ class Nature(Store, RpcMixin):
 
 
 class Document(Store, RpcMixin):
+    interface = 'WSDocMgmt'
+
+    class Author(Store):
+        first_name = Field(target='firstName')
+        last_name = Field(target='lastName')
+
+    class Attributes(Store):
+        author = Field(target='author')
+        as_epub = BooleanField(target='available_as_epub') # should be deprecated soon
+        as_pdf = BooleanField(target='available_as_pdf') # should be deprecated soon
+        as_watermark = BooleanField(target='available_as_watermark') # should be deprecated soon
+        via_iap = BooleanField(target='available_via_iap') # should be deprecated soon
+        with_adobe_drm = BooleanField(target='available_with_adobe_drm') # should be deprecated soon
+        hash = Field(target='binary_hash')
+        content_provider_name = Field(target='content_provider_name')
+        content_provider_id = Field(target='content_provider_specific_id')
+        content_source_id = Field(target='content_source_id')
+        cover_ratio = FloatField(target='cover_image_aspect_ratio')
+        currency = Field(target='currency')
+        first_publication = Field(target='date_of_first_publication')
+        description = Field(target='description')
+        fulfillment_id = Field(target='fulfillment_id')
+        fulfillment_type = Field(target='fulfillment_type')
+        isbn = LongIntField(target='isbn')
+        language = Field(target='language')
+        pages = IntegerField(target='number_of_pages')
+        price = FloatField(target='price')
+        publication_date = DateField(target='publication_date')
+        publisher = Field(target='publisher')
+        size = IntegerField(target='size')
+        tax_group = Field(target='tax_group')
+        title = Field(target='title')
+        year = IntegerField(target='year')
+        large_cover_url = Field(target='cover_image_url_large')
+        normal_cover_url = Field(target='cover_image_url_normal')
+        medium_cover_url = Field(target='cover_image_url_medium')
+
+    # probably should be moved outside of this class
+    class Category(Store):
+        id = Field(target='ID')
+        name = Field(target='name')
+        offset = IntegerField(target='offset')
+        count = IntegerField(target='count')
+        subtree_size = IntegerField(target='subtreeSize')
+        children = Field(target='childrenIDs')
+        parent = Field(target='parentID')
+        filter = Field(target='filter')
+
+    class License(Store):
+        key = Field(target='key')
+        user_roles = Field(target='currentUserRoles')
+
     id = Field(target='documentID')
+    master_id = Field(target='documentMasterID')
+    name = Field(target='displayName')
+    lang_code = Field(target='languageCode')
+    format = Field(target='format')
+    version = IntegerField(target='version')
+    attributes = EmbeddedStoreField(target='attributes', store_class=Attributes)
+    votes = IntegerField(target='numberOfVotes')
+    modification_date = DateField(target='modificationTime')
+    creation_date = DateField(target='creationTime')
+    owner = Field(target='owner')
+    creator = Field(target='creator')
+    in_public_list = BooleanField(target='inPublicList')
+    version_size = IntegerField(target='versionSize')
+    version_access_type = Field(target='versionAccessType')
+    has_thumbnail = BooleanField(target='hasThumbnail')
+    licenses = EmbeddedStoreField(target='licenses', store_class=License, is_array=True)
+    category_ids = Field(target='contentCategoryIDs')
+    categories = EmbeddedStoreField(target='contentCategories', store_class=Category, is_array=True)
+    drm_type = Field(target='drmType')
+    catalog_state = Field(target='catalogDocumentState')
+    authors = EmbeddedStoreField(target='authors', store_class=Author, is_array=True)
+
+    @property
+    def price(self):
+        return Money(amount=self.attributes.price, currency=self.attributes.currency)
+
+
+class ReaktorMoney(Store):
+    """Helper class to use with the new reaktor price fields."""
+    amount = FloatField(target='amount')
+    currency = Field(target='currency')
 
 
 class Voucher(Store, RpcMixin):
@@ -25,10 +109,26 @@ class Voucher(Store, RpcMixin):
 
 class Item(Store):
     """Base item class, to be extended for specific purposes."""
-    total = MoneyField(target='positionTotal')
-    net_total = MoneyField(target='positionNetTotal')
-    tax_total = MoneyField(target='positionTaxTotal')
-    undiscounted_total = MoneyField(target='undiscountedPositionTotal')
+    _total = EmbeddedStoreField(target='positionTotal', store_class=ReaktorMoney)
+    _net_total = EmbeddedStoreField(target='positionNetTotal', store_class=ReaktorMoney)
+    _tax_total = EmbeddedStoreField(target='positionTaxTotal', store_class=ReaktorMoney)
+    _undiscounted_total = EmbeddedStoreField(target='undiscountedPositionTotal', store_class=ReaktorMoney)
+
+    @property
+    def total(self):
+        return Money(amount=self._total.amount, currency=self._total.currency)
+
+    @property
+    def net_total(self):
+        return Money(amount=self._net_total.amount, currency=self._net_total.currency)
+
+    @property
+    def tax_total(self):
+        return Money(amount=self._tax_total.amount, currency=self._tax_total.currency)
+
+    @property
+    def undiscounted_total(self):
+        return Money(amount=self._undiscounted_total.amount, currency=self._undiscounted_total.currency)
 
 
 class DocumentItem(Item):
@@ -73,12 +173,28 @@ class Basket(Store, RpcMixin):
     creation_date = DateField(target='creationTime')
     modification_date = DateField(target='modificationTime')
     country = Field(target='country')
-    total = MoneyField(target='total')
-    net_total = MoneyField(target='netTotal')
-    tax_total = MoneyField(target='taxTotal')
-    undiscounted_total = MoneyField(target='undiscountedTotal')
+    _total = EmbeddedStoreField(target='total', store_class=ReaktorMoney)
+    _net_total = EmbeddedStoreField(target='netTotal', store_class=ReaktorMoney)
+    _tax_total = EmbeddedStoreField(target='taxTotal', store_class=ReaktorMoney)
+    _undiscounted_total = EmbeddedStoreField(target='undiscountedTotal', store_class=ReaktorMoney)
     # payment_props = EmbeddedStoreField(target='paymentProperties', store_class=Payment)
     items = EmbeddedStoreField(target='positions', store_class=item_factory, is_array=True)
+
+    @property
+    def total(self):
+        return Money(amount=self._total.amount, currency=self._total.currency)
+
+    @property
+    def net_total(self):
+        return Money(amount=self._net_total.amount, currency=self._net_total.currency)
+
+    @property
+    def tax_total(self):
+        return Money(amount=self._tax_total.amount, currency=self._tax_total.currency)
+
+    @property
+    def undiscounted_total(self):
+        return Money(amount=self._undiscounted_total.amount, currency=self._undiscounted_total.currency)
 
     @classmethod
     @rpc_call
