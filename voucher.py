06069c7785bc2a16702b700c348fed2129c48be3
Iurii Kudriavtsev <iurii.kudriavtsev@txtr.com>
2014-02-06 09:39:57 +0100

    SKINSTXTRCOM-4163 | splitting barrel models into separate files.

---
+++ voucher.py
@@ -0,0 +1,23 @@
+from . import Store, Field, LongIntField, EmbeddedStoreField
+from models import ReaktorMoney
+from rpc import RpcMixin
+from money import Money
+
+
+# FIXME: this class is used as embedded for `voucherApplications`
+# thus it might differ from the voucher that comes in the basket position
+class Voucher(Store, RpcMixin):
+    code = LongIntField(target='code')
+    text = Field(target='text')
+    _initial_amount = EmbeddedStoreField(target='initialAmount', store_class=ReaktorMoney)
+    _amount = EmbeddedStoreField(target='amount', store_class=ReaktorMoney)
+    # not sure if it is needed
+    # java_cls = Field(target='javaClass')
+
+    @property
+    def initial_amount(self):
+        return Money(amount=self._initial_amount.amount, currency=self._initial_amount.currency)
+
+    @property
+    def amount(self):
+        return Money(amount=self._amount.amount, currency=self._amount.currency)
