247c76b5e8671e36f21efbd91cc93a718ca7af4b
Iurii Kudriavtsev <iurii.kudriavtsev@txtr.com>
2014-02-11 10:44:48 +0100

    SKINSTXTRCOM-4163 | No need to wrap every call in `rpc_call` - the signature is wrapped now.

--- rpc.py
+++ rpc.py
@@ -13,17 +13,6 @@ def check_data(data_converter, dct):
         return data_converter(dct)
 
 
-class RpcMixin(object):
-    @classmethod
-    def signature(cls, interface=None, method=None, data_converter=None, args=None):
-        """Returns a named tuple suitable for easy RPC call while providing
-        some defaults: the RPC interface and the data converter are read
-        from the class.
-        """
-        return RpcSignature(interface=interface or cls.interface, method=method,
-                            data_converter=data_converter or cls, args=args)
-
-
 def rpc_call(func):
     """Handle RPC calls using the wrapped classmethod returned signature.
     Handles multiple rpc signatures and returns the last result.
@@ -39,6 +28,18 @@ def do_rpc_call(sig):
     interface = getattr(reaktor(), sig.interface)
     converter = partial(check_data, sig.data_converter)
     return getattr(interface, sig.method)(*sig.args, data_converter=converter)
+
+
+class RpcMixin(object):
+    @classmethod
+    @rpc_call
+    def signature(cls, interface=None, method=None, data_converter=None, args=None):
+        """Returns a named tuple suitable for easy RPC call while providing
+        some defaults: the RPC interface and the data converter are read
+        from the class.
+        """
+        return RpcSignature(interface=interface or cls.interface, method=method,
+                            data_converter=data_converter or cls, args=args)
 
 
 def deprecated(deprecated_call):
